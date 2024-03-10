ee15f107dc5d5d2fc411ea0031015afadcab4512
Kavin Phan <kavin@kphan.tech>
2024-03-10 09:47:25 -0400

    Add piano layout

--- src/screens/Piano.jsx
+++ src/screens/Piano.jsx
@@ -1,32 +1,36 @@
 import { useEffect } from "react";
 
 const NOTES = [
-  { name: "c_0", key: "z" },
-  { name: "c_0_sharp", key: "s" },
-  { name: "d_0", key: "x" },
-  { name: "d_0_sharp", key: "d" },
-  { name: "e_0", key: "c" },
-  { name: "f_0", key: "v" },
-  { name: "f_0_sharp", key: "g" },
-  { name: "g_0", key: "b" },
-  { name: "g_0_sharp", key: "h" },
-  { name: "a_0", key: "n" },
-  { name: "a_0_sharp", key: "j" },
-  { name: "b_0", key: "m" },
-  { name: "c_1", key: "," },
+  { name: "c_0", key: "z", label: "C" },
+  { name: "c_0_sharp", key: "s", label: "C#" },
+  { name: "d_0", key: "x", label: "D" },
+  { name: "d_0_sharp", key: "d", label: "D#" },
+  { name: "e_0", key: "c", label: "E" },
+  { name: "f_0", key: "v", label: "F" },
+  { name: "f_0_sharp", key: "g", label: "F#" },
+  { name: "g_0", key: "b", label: "G" },
+  { name: "g_0_sharp", key: "h", label: "G#" },
+  { name: "a_0", key: "n", label: "A" },
+  { name: "a_0_sharp", key: "j", label: "A#" },
+  { name: "b_0", key: "m", label: "B" },
+  { name: "c_1", key: ",", label: "C" },
 ];
 
 export default function Piano() {
-  const playSound = (name) => () => {
+  const playSound = (btn, name) => {
     createjs.Sound.play(`piano.${name}`);
+
+    setTimeout(() => btn.blur(), 200);
   };
 
   useEffect(() => {
     const handleKeyPress = (e) => {
       const note = NOTES.find((note) => note.key === e.key);
-      if (!note) return;
+      if (!note || e.repeat) return;
 
-      createjs.Sound.play(`piano.${note.name}`);
+      const btn = document.getElementById(`${note.name}-btn`);
+      btn?.focus();
+      btn?.click();
     };
 
     document.addEventListener("keypress", handleKeyPress);
@@ -35,16 +39,97 @@ export default function Piano() {
   }, []);
 
   return (
-    <main>
-      {NOTES.map((note) => {
-        const { name, key } = note;
+    <main className="p-8 flex flex-col justify-between items-center">
+      <div className="h-[50vh] flex flex-col justify-center text-center">
+        <p className="text-xl font-bold">cat pic here</p>
+      </div>
+      <div>
+        <div className="grid grid-cols-16 gap-2">
+          <div className="col-span-1 h-0" />
+          {NOTES.filter((note) => note.name.endsWith("_sharp"))
+            .slice(0, 2)
+            .map((note) => {
+              const { name, key, label } = note;
 
-        return (
-          <button key={name} onClick={playSound(name)}>
-            {key}
-          </button>
-        );
-      })}
+              return (
+                <div key={name} className="col-span-2 flex justify-center">
+                  <button
+                    id={`${name}-btn`}
+                    onClick={(e) => playSound(e.target, name)}
+                    className="group outline-none"
+                  >
+                    <p className="font-semibold text-gray-600">{label}</p>
+                    <img
+                      src={`/icons/${key}.svg`}
+                      alt="z-button"
+                      className="group-active:hidden group-focus:hidden"
+                    />
+                    <img
+                      src={`/icons/${key}_press.svg`}
+                      alt="z-button"
+                      className="hidden group-active:block group-focus:block"
+                    />
+                  </button>
+                </div>
+              );
+            })}
+          <div className="col-span-2 h-0" />
+          {NOTES.filter((note) => note.name.endsWith("_sharp"))
+            .slice(2)
+            .map((note) => {
+              const { name, key, label } = note;
+
+              return (
+                <div key={name} className="col-span-2 h-36 flex justify-center">
+                  <button
+                    id={`${name}-btn`}
+                    onClick={(e) => playSound(e.target, name)}
+                    className="group outline-none"
+                  >
+                    <p className="font-semibold text-gray-600">{label}</p>
+                    <img
+                      src={`/icons/${key}.svg`}
+                      alt="z-button"
+                      className="group-active:hidden group-focus:hidden"
+                    />
+                    <img
+                      src={`/icons/${key}_press.svg`}
+                      alt="z-button"
+                      className="hidden group-active:block group-focus:block"
+                    />
+                  </button>
+                </div>
+              );
+            })}
+        </div>
+        <div className="grid grid-cols-8 gap-2">
+          {NOTES.filter((note) => !note.name.endsWith("_sharp")).map((note) => {
+            const { name, key, label } = note;
+
+            return (
+              <div key={name} className="flex justify-center">
+                <button
+                  id={`${name}-btn`}
+                  onClick={(e) => playSound(e.target, name)}
+                  className="group outline-none"
+                >
+                  <p className="font-semibold text-gray-600">{label}</p>
+                  <img
+                    src={`/icons/${key}.svg`}
+                    alt="z-button"
+                    className="group-active:hidden group-focus:hidden"
+                  />
+                  <img
+                    src={`/icons/${key}_press.svg`}
+                    alt="z-button"
+                    className="hidden group-active:block group-focus:block"
+                  />
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </main>
   );
 }
