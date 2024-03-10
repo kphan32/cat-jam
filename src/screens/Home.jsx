8adf2de6c782bb379efa806fedd005b82228a5b3
minimatime <nvalluri3@gatech.edu>
2024-03-10 03:28:20 -0400

    add icons for instructions screen, add code for background code and implemented in home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col justify-center items-center">
+    <main className="h-screen flex flex-col justify-center items-center bg-green">
       <h1 className="col-span-2 text-4xl">Cat Jam</h1>
       <div className="p-4 grid grid-cols-2 gap-4">
         <Link to="/instruments">
