69edfd7c006dba599622fbe95ba51a4457107e17
Kavin Phan <kavin@kphan.tech>
2024-03-10 01:06:49 -0500

    Fix responsiveness of home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col justify-center items-center gap-4">
-      <h1 className="text-4xl">Cat Jam</h1>
-      <div className="flex justify-center items-center gap-4">
+    <main className="h-screen flex flex-col justify-center items-center">
+      <h1 className="col-span-2 text-4xl">Cat Jam</h1>
+      <div className="p-4 grid grid-cols-2 gap-4">
         <Link to="/instruments">
-          <img src= "/icons/start.svg"></img>
+          <img src="/icons/start.svg"></img>
         </Link>
-        <img src= "/icons/help.svg"></img>
+        <img src="/icons/help.svg"></img>
       </div>
     </main>
   );
