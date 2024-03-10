06f665c84f0eebcc091738b86226fe9093df5ed4
Kavin Phan <kavin@kphan.tech>
2024-03-10 10:31:56 -0400

    Add cat to home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <main className="h-screen flex flex-col justify-center items-center bg-green">
+      <img src="/icons/cat.svg" className="mb-4 w-48" />
       <h1 className="text-8xl text-white">Cat Jam</h1>
       <div className="p-4 grid grid-rows-2 gap-4">
         <Link to="/instruments">
-          <img src="/icons/start.svg"></img>
+          <img src="/icons/start.svg" />
         </Link>
-        <img src="/icons/help.svg"></img>
+        <img src="/icons/help.svg" />
       </div>
     </main>
   );
